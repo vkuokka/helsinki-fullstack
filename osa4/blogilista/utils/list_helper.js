c8d0c3d520fd6bc8e8a56ab66d6da0039a411a7b
Vesa Kuokka <vkuokka@student.hive.fi>
2020-09-28 17:46:45 +0300

    4.5: apufunktioita ja yksikkotesteja

---
+++ osa4/blogilista/utils/list_helper.js
@@ -0,0 +1,30 @@
+_ = require('lodash')
+
+const dummy = (blogs) => {
+	return 1
+}
+
+const totalLikes = (blogs) => {
+	return blogs.reduce((sum, blog) => {
+		return sum + blog.likes
+	}, 0)
+}
+
+const favouriteBlog = (blogs) => {
+	return blogs.reduce((mostLiked, blog) => {
+		return mostLiked == null || mostLiked.likes < blog.likes
+			? blog
+			: mostLiked
+	}, null)
+}
+
+const mostBlogs = (blogs) => {
+	return null
+}
+
+module.exports = {
+	dummy,
+	totalLikes,
+	favouriteBlog,
+	mostBlogs,
+}
